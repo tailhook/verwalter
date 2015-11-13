e04774a94ee35407fa96a87b00587d40f624921d
Paul Colomiets <paul@colomiets.name>
2015-11-13 16:57:39 +0200

    Minimum static serving implemented

--- src/net.rs
+++ src/net.rs
@@ -1,4 +1,8 @@
 use std::io;
+use std::io::Read;
+use std::path::Path;
+use std::path::Component::ParentDir;
+use std::fs::File;
 use std::net::SocketAddr;
 
 use rotor;
@@ -8,6 +12,7 @@ use rotor_http::http1::{Client, Request, Handler, ResponseBuilder};
 use mio::EventLoop;
 use mio::tcp::TcpListener;
 use hyper::uri::RequestUri::{AbsolutePath};
+use hyper::status::StatusCode;
 
 use routing_util::path_component;
 
@@ -20,6 +25,24 @@ pub enum Route {
 }
 
 struct Public;
+
+fn read_file<P:AsRef<Path>>(path: P, res: &mut ResponseBuilder)
+    -> Result<(), io::Error>
+{
+    let path = path.as_ref();
+    for cmp in path.components() {
+        if matches!(cmp, ParentDir) {
+            return Err(io::Error::new(io::ErrorKind::PermissionDenied,
+                "Parent dir `..` path components are not allowed"));
+        }
+    }
+    let mut file = try!(File::open(path));
+    let mut buf = Vec::with_capacity(1024);
+    try!(file.read_to_end(&mut buf));
+    res.set_status(StatusCode::Ok);
+    res.put_body(buf);
+    Ok(())
+}
 
 impl Handler<Context> for Public {
     fn request(req: Request, res: &mut ResponseBuilder, ctx: &mut Context) {
@@ -35,6 +58,25 @@ impl Handler<Context> for Public {
             _ => return,   // Do nothing: not found or bad request
         };
         debug!("Routed {:?} to {:?}", req, route);
+        let iores = match route {
+            Index => read_file("public/index.html", res),
+            Static(ref x) => read_file(format!("public/{}", &x[1..]), res),
+        };
+        match iores {
+            Ok(()) => {}
+            Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+                return; // 404 by default
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::PermissionDenied => {
+                res.set_status(StatusCode::Forbidden);
+                return;
+            }
+            Err(e) => {
+                info!("Error serving {:?}: {}", route, e);
+                res.set_status(StatusCode::Forbidden);
+                return;
+            }
+        }
     }
 }
 
