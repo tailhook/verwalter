e70da3defe0f7fe4a9e77678575bbecd31ed6965
Paul Colomiets <paul@colomiets.name>
2016-09-22 16:07:15 +0300

    Fix calculating number of processes when they are not on all nodes

--- frontend/util/schedule.js
+++ frontend/util/schedule.js
@@ -2,8 +2,10 @@ export function total_processes(schedule, role_name) {
     let num = 0;
     for(let host in schedule.nodes) {
         let hrole = schedule.nodes[host].roles[role_name]
-        for(let kind in hrole.daemons) {
-            num += hrole.daemons[kind].instances || 0
+        if(hrole) {
+            for(let kind in hrole.daemons) {
+                num += hrole.daemons[kind].instances || 0
+            }
         }
     }
     return num
