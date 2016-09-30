d2bbe17ada1d5207289baf4aba0dd5e8d1338bf8
Paul Colomiets <paul@colomiets.name>
2016-09-30 18:33:46 +0300

    /services: add a link to show all servers of given role

--- frontend/util/services.js
+++ frontend/util/services.js
@@ -1,2 +1,11 @@
-export function role_names(schedule) {
+export function role_nodes(schedule, role) {
+    let res = {}
+    for(let node_name in schedule.data.nodes) {
+        let node = schedule.data.nodes[node_name];
+        let val = node.roles && node.roles[role]
+        if(val) {
+            res[node_name] = val
+        }
+    }
+    return res
 }
