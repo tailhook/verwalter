2055382960bbb0ac85667799d5c318a4b9eed0af
Paul Colomiets <paul@colomiets.name>
2016-10-07 14:03:05 +0300

    Better display for some time-based metrics

--- frontend/util/str.js
+++ frontend/util/str.js
@@ -2,3 +2,7 @@ export function ends_with(x, value) {
     let dist = x.length - value.length
     return dist >= 0 && x.lastIndexOf(value) == dist
 }
+
+export function starts_with(x, value) {
+    return x.length >= value.length && x.indexOf(value) == 0
+}
