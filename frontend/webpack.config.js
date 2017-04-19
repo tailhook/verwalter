f0b41746bffac35d512b7aba83e8cfc91f43a7be
Paul Colomiets <paul@colomiets.name>
2017-04-19 17:00:19 +0300

    Removes dev-server support from webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,11 +3,7 @@ var BabiliPlugin = require('babili-webpack-plugin')
 var DEV = process.env['NODE_ENV'] != 'production';
 module.exports = {
     context: __dirname,
-    entry: DEV ? [
-        "./index",
-        "webpack-dev-server/client?http://localhost:8080",
-        "webpack/hot/only-dev-server",
-    ] : "./index",
+    entry: "./index",
     output: {
         path: __dirname + "/../public/js",
         filename: "bundle.js",
