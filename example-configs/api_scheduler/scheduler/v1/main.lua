cfd9692d34da36ab822f101ede752e38f5e18659
Paul Colomiets <paul@colomiets.name>
2017-04-21 18:14:59 +0300

    api_scheduler: update toolbox

--- example-configs/api_scheduler/scheduler/v1/main.lua
+++ example-configs/api_scheduler/scheduler/v1/main.lua
@@ -1,7 +1,18 @@
+local log = require("toolbox/modules/log")
+-- local merge = require("toolbox/modules/merge")
+local roles_from_state = require("toolbox/modules/role").from_state
+
 local function scheduler(state)
-    return "{}", "Empty"
+    local roles = roles_from_state(state)
+    for _, role in pairs(roles) do
+        -- role.independent_scheduling()
+    end
+    return {state={}}
+    -- return merge.schedules(func.map_pairs(function (_, role)
+    --     return role.output()
+    -- end, roles))
 end
 
 return {
-    scheduler=scheduler,
+    scheduler=log.wrap_scheduler(scheduler),
 }
