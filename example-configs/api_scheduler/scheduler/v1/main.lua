bc38ad7b9f872d478a9b5987a3be19b9eaabb48c
Paul Colomiets <paul@colomiets.name>
2017-04-25 16:04:48 +0300

    api_scheduler: displays the role in UI

--- example-configs/api_scheduler/scheduler/v1/main.lua
+++ example-configs/api_scheduler/scheduler/v1/main.lua
@@ -1,16 +1,14 @@
 local log = require("toolbox/modules/log")
 -- local merge = require("toolbox/modules/merge")
 local roles_from_state = require("toolbox/modules/role").from_state
+local merge_output = require("toolbox/modules/role").merge_output
 
 local function scheduler(state)
     local roles = roles_from_state(state)
     for _, role in pairs(roles) do
-        -- role.independent_scheduling()
+        role.independent_scheduling()
     end
-    return {state={}}
-    -- return merge.schedules(func.map_pairs(function (_, role)
-    --     return role.output()
-    -- end, roles))
+    return merge_output(roles)
 end
 
 return {
