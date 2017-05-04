b419997f0061bbe8e1e72ae291752f7f5eedd022
Paul Colomiets <paul@colomiets.name>
2017-05-04 13:42:37 +0300

    Simplified `main.lua` even more (via new toolbox)

--- example-configs/api_scheduler/scheduler/v1/main.lua
+++ example-configs/api_scheduler/scheduler/v1/main.lua
@@ -1,13 +1,12 @@
 local log = require("toolbox/modules/log")
--- local merge = require("toolbox/modules/merge")
 local roles_from_state = require("toolbox/modules/role").from_state
 local merge_output = require("toolbox/modules/role").merge_output
+local api = require('toolbox/modules/drivers/api.lua')
 
 local function scheduler(state)
-    local roles = roles_from_state(state)
-    for _, role in pairs(roles) do
-        role.independent_scheduling()
-    end
+    local roles = roles_from_state { state,
+        driver=function(_) return api end,
+    }
     return merge_output(roles)
 end
 
