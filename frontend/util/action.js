f3fb146e9f5f73c1c36e10e38c58f96520885f2c
Paul Colomiets <paul@colomiets.name>
2017-06-19 20:12:10 +0300

    Implements buttons in smooth update

--- frontend/util/action.js
+++ frontend/util/action.js
@@ -5,3 +5,16 @@ export function execute(data) {
     }).then(response => console.log("Action response", response))
     return {type: 'execute_action', data: data}
 }
+
+export function compare_action(act, rule) {
+    let btn = act.button
+    if(!btn) {
+        return false;
+    }
+    for(var k in rule) {
+        if(btn[k] != rule[k]) {
+            return false;
+        }
+    }
+    return true;
+}
