b67aa70ff257e7440c94b3688bb08a93785ab9cb
Paul Colomiets <paul@colomiets.name>
2017-09-22 10:20:22 +0000

    Version bumped to v0.8.21

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
         }),
         new webpack.NoEmitOnErrorsPlugin(),
         new webpack.DefinePlugin({
-            VERSION: JSON.stringify("v0.8.20"),
+            VERSION: JSON.stringify("v0.8.21"),
             "process.env.NODE_ENV": JSON.stringify(process.env['NODE_ENV']),
             DEBUG: DEV,
         }),
