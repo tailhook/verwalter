e259a20edd58873993d4f148de60b69c4c403be9
Paul Colomiets <paul@colomiets.name>
2017-09-22 13:16:19 +0300

    Better communication between verwalter and verwalter_render
    Now stores schedule in temporary file if certain size of schedule is
    reached

--- src/daemon/fs_util.rs
+++ src/daemon/fs_util.rs
@@ -1,7 +1,7 @@
 use std::io::{self, Write, Read, BufWriter};
 use std::path::Path;
 use std::io::ErrorKind::{NotFound, AlreadyExists};
-use std::fs::{File, create_dir, rename, metadata};
+use std::fs::{File, OpenOptions, remove_file, create_dir, rename, metadata};
 
 use rustc_serialize::{Encodable};
 use rustc_serialize::json::{as_pretty_json, Json};
@@ -30,6 +30,21 @@ pub fn read_json(path: &Path) -> io::Result<Json> {
     try!(file.read_to_string(&mut buf));
     Json::from_str(&buf).map_err(
         |_| io::Error::new(io::ErrorKind::InvalidData, "Can't decode json"))
+}
+
+pub fn safe_write(path: &Path, data: &[u8]) -> io::Result<()> {
+    ensure_dir(&path.parent().expect("valid dir"))?;
+    let tmppath = path.with_extension("tmp");
+    match remove_file(&tmppath) {
+        Ok(()) => {}
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {}
+        Err(e) => return Err(e),
+    }
+    let mut file = OpenOptions::new().create_new(true).write(true)
+        .open(&tmppath)?;
+    file.write_all(data)?;
+    rename(&tmppath, path)?;
+    Ok(())
 }
 
 pub fn write_file<T: Encodable>(path: &Path, data: &T) -> io::Result<()> {
