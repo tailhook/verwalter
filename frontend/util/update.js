aefb96aac2c6cd79d20b4553d968664d032fe11d
Paul Colomiets <paul@colomiets.name>
2017-12-28 18:34:20 +0200

    Eliminates "Invalid update" message on end of update

--- frontend/util/update.js
+++ frontend/util/update.js
@@ -1,10 +1,14 @@
 export function step_and_index(pipeline, step_name) {
-    if(step_name == 'start') {
+    if(step_name == 'start' || step_name == 'revert_done') {
         return [{name: 'start'}, -1]
+    }
+    if(step_name == 'done') {
+        return [{name: 'done'}, pipeline.length]
     }
     for(let i = 0; i < pipeline.length; ++i) {
         if(pipeline[i].name == step_name) {
             return [pipeline[i], i];
         }
     }
+    console.error("Invalid step", step_name, "in pipeline", pipeline)
 }
