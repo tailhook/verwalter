60073687bfabeb3113319a885ca41e00ee253d48
Paul Colomiets <paul@colomiets.name>
2018-01-04 14:25:04 +0000

    Version bumped to v0.9.13

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
         }),
         new webpack.NoEmitOnErrorsPlugin(),
         new webpack.DefinePlugin({
-            VERSION: JSON.stringify("v0.9.12"),
+            VERSION: JSON.stringify("v0.9.13"),
             "process.env.NODE_ENV": JSON.stringify(process.env['NODE_ENV']),
             DEBUG: DEV,
         }),
